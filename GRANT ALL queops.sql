4edef7a7384fde907a9d1868b98cfd55ab20cec3
Claudemir Carvalho <32469935+claudemircarvalho91@users.noreply.github.com>
2017-10-23 13:19:19 -0200

    Alteração de permição
    1.1 Foi feito o backup do banco com o user padrao, para usar no sistema alterar a permissao GRANT ALL queops.sql

---
+++ GRANT ALL queops.sql
@@ -0,0 +1,19 @@
+GRANT ALL ON SEQUENCE public.categorias_codigo_seq TO queops WITH GRANT OPTION;
+
+GRANT ALL ON SEQUENCE public.usuarios_id_seq TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public."BaixaBemPatrimonial" TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public."BemPatrimonial" TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public.categorias TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public.departamentos TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public.mbp TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public.predios TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public.salas TO queops WITH GRANT OPTION;
+
+GRANT ALL ON TABLE public.usuarios TO queops WITH GRANT OPTION;
