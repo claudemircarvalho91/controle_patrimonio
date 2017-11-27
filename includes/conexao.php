8eb2567cbfbb36cb90a8a6372dc26f92ab146db0
Claudemir Carvalho <32469935+claudemircarvalho91@users.noreply.github.com>
2017-11-27 16:41:33 -0200

    Conexao com banco
    Arquivo completo
    *concexao atual
    *Parte Final

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,1 +1,36 @@
-dfasfasdfads
+<?
+
+$titulo_global = 'Sistema de Controle de Patrim&ocirc;nio';
+
+@date_default_timezone_set('America/Sao_Paulo');
+
+if($_SERVER['REMOTE_ADDR'] != '127.0.0.1' && $_SERVER['REMOTE_ADDR'] != '::1'){
+
+   @define('ROOT','http://localhost:8080/patrimonio');
+   @define('ROOT_ADMIN','http://localhost:8080/patrimonio');
+   
+   $server_db  =  'localhost';
+   $usuario_db =  'queops';
+   $senha_db   =  'piramide';
+   $banco_db   =  'patrimonio';
+   
+}else{
+   
+   @define('ROOT','http://localhost:8080/patrimonio');
+   @define('ROOT_ADMIN','http://localhost:8080/patrimonio');
+   
+
+   $server_db  =  'localhost';
+   $usuario_db =  'queops';
+   $senha_db   =  'piramide';
+   $banco_db   =  'patrimonio';
+
+}
+
+$conexao = @pg_connect ("host=".$server_db." dbname=".$banco_db." port=5432 user=".$usuario_db." password=".$senha_db."");
+
+if(!$conexao){
+   print "<p class=\"status\"><font class=\"status\">Não foi possível estabelecer uma conexão com o banco de dados.</p>";
+}
+
+?>
