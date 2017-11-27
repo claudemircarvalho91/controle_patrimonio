013ba1da6b5d7bb014ada2d588c8b61654eb66b8
rafaelcouto1 <33104820+rafaelcouto1@users.noreply.github.com>
2017-11-27 09:14:56 -0200

    erro

---
+++ erro.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>ERRO!</title>
+</head>
+
+<body>
+
+<div style="position:absolute; width:700px; height:410px; left:50%; top:50%; margin-left:-350px; margin-top:-205px;"><img src="<?=ROOT_ADMIN?>/imagens/pagina-erro-404.gif" alt="" /></div>
+
+</body>
+</html>
